086133b521728e1be49eea203dbab47705fb94bc
Luis Lopez <luigibyte@gmail.com>
2016-10-20 14:39:37 -0300

    configure: network, currency, version

--- lib/enigma/config.rb
+++ lib/enigma/config.rb
@@ -1,9 +1,12 @@
 module Enigma
   class Config
-    attr_accessor :api_key
+    attr_accessor :api_key, :version, :currency, :network
 
     def initialize
       @api_key = ""
+      @version = "v1"
+      @currency = "btc"
+      @network = "main"
     end
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,7 +8,14 @@ class EnigmaTest < Minitest::Test
   def test_configure
     Enigma.configure do |config|
       config.api_key = "APIKEY"
+      config.version = "v2"
+      config.currency = "bcy"
+      config.network = "test"
     end
+    
     assert_equal "APIKEY", Enigma.config.api_key
+    assert_equal "v2", Enigma.config.version
+    assert_equal "bcy", Enigma.config.currency
+    assert_equal "test", Enigma.config.network
   end
 end
